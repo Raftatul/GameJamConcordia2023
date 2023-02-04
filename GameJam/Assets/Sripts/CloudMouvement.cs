c9b5dad1b163b29de69d3f953ccfdb44fe5a4005
vivianb11 <vivianb.info@gmail.com>
2023-02-04 16:59:25 -0500

    Cloud finished (maybe)
    You can now add to a list different type of clouds respecting the cloud prefab it fades away when the cloud event is over

--- GameJam/Assets/Sripts/CloudMouvement.cs
+++ GameJam/Assets/Sripts/CloudMouvement.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class CloudMouvement : MonoBehaviour
 {
     [SerializeField] private float mouvSpeed = 40;
+    [SerializeField] private Image selfCloud;
+    private bool fade = false;
 
     private void FixedUpdate()
     {
         this.transform.position += new Vector3(-1 * mouvSpeed * Time.deltaTime,0,0);
+
+        if (!GlobalVariable.clouds && !fade)
+        {
+            fade = true;
+            selfCloud.DOFade(0, 3f).OnComplete(() => {
+                Destroy(gameObject); 
+            });
+        }
     }
 
     private void OnBecameInvisible()
