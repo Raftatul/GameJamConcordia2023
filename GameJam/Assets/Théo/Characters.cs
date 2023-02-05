194c4ea5429b1bc54d1fb629cbf9ef39688a54a8
Theonojutsu <tbirlian@gmail.com>
2023-02-04 22:34:35 -0500

    Théoooooooooowww

--- GameJam/Assets/Théo/Characters.cs
+++ GameJam/Assets/Théo/Characters.cs
@@ -1,41 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Characters : MonoBehaviour
 {
-	#region Variable
+	#region Variables
 
-	public float speedCharacter = 5f;
-	public float frequency = 20f;
-	public float magnitude = 0.5f;
-	Vector3 pos;
-
-	public float leftLimit;
-	public float currentTime = 0;
-	public float timer;
-	private Rigidbody2D headingPointRigidBody; // RigidBody de ma cible
+	public float speedCharacter = 50;
+	public float duration = 10^4;
+	public float strength = 1;
+	public int vibrato = 0;
+	public float randomness = 0;
+	public bool snapping = false;
+	public bool fadeOut = true;
+	public ShakeRandomnessMode randomnessMode;
+	
 	private Rigidbody2D characterRigidbody; // RigidBody de mon personnage
-	public GameObject cheatTeleport; // Correspond à un gameObject vide que j'utilise pour déplacer mon personnage dessus. Je l'utilise pour faire des essais et itérer
-	public GameObject headingPoint;
-	public GameObject character;
 
-	#endregion Variable
-
-	void Start()
-	{
-		pos = transform.position;
-	}
+	#endregion Variables
 
 	void Update()
 	{
-		MoveLeft();
+        DotWeen();
 
-		//MovingGameObjects(character);
+        Vector3 posHeadingPoint = new Vector3(transform.position.x + 3, transform.position.y + 3, transform.position.z);
+        transform.position = posHeadingPoint;
 
-		headingPoint.transform.SetPositionAndRotation(new Vector3(character.transform.position.x + 3, character.transform.position.y + 3, headingPointRigidBody.transform.position.z), Quaternion.Euler(Vector2.zero));
-
-		Vector3 direction = headingPoint.transform.position - this.transform.position;
+        Vector3 direction = posHeadingPoint - transform.position;
 		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 		characterRigidbody.rotation = angle;
 		direction.Normalize();
@@ -43,18 +35,12 @@ public class Characters : MonoBehaviour
 		MoveCharacter(movement);
 	}
 
-	void MoveLeft()
-	{
-		pos -= transform.right * Time.deltaTime * speedCharacter;
-		transform.position = pos + transform.up * Mathf.Sin(Time.time * frequency) * magnitude;
-	}
+    void DotWeen()
+    {
+        transform.DOShakePosition(duration, strength, vibrato, randomness, snapping = false, fadeOut = true, randomnessMode = ShakeRandomnessMode.Full);
+    }
 
-	//public void MovingGameObjects(GameObject a)
-	//{
-	//	a.transform.Translate((speedCharacter) * Time.deltaTime, 0, 0);
-	//}
-
-	void MoveCharacter(Vector2 direction)
+    void MoveCharacter(Vector2 direction)
 	{
 		characterRigidbody.MovePosition((Vector2)transform.position + (direction * (speedCharacter) * Time.deltaTime));
 	}
