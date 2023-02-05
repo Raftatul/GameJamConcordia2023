7326c5ea758d5654a2fe05a6171cf6ed476a6778
UserJoshua <104460878+UserJoshua@users.noreply.github.com>
2023-02-05 13:02:18 -0500

    air ressources + audio / event weather

--- GameJam/Assets/RessourcesHarvester.cs
+++ GameJam/Assets/RessourcesHarvester.cs
@@ -5,6 +5,8 @@ using DG.Tweening;
 
 public class RessourcesHarvester : MonoBehaviour
 {
+    [SerializeField] float AirGain = 1;
+
     public PlayerRessources Player;
     public float IncomeTime;
 
@@ -32,8 +34,22 @@ public class RessourcesHarvester : MonoBehaviour
 
         foreach (var item in tree.leafs)
         {
-            item.transform.DOScale(new Vector3(1.2f,1.2f,1.2f), 1).SetEase(Ease.OutElastic).SetLoops(3, LoopType.Yoyo);
+            Vector3 to = new Vector3(Random.Range(-0.2f, 0.2f), Random.Range(0.3f, 0.5f));
+            //item.transform.DOScale(new Vector3(1.2f,1.2f,1.2f), 1).SetEase(Ease.OutElastic).SetLoops(3, LoopType.Yoyo);
+            //item.transform.DOShakePosition(1, 0.5, 5, 45, false, true).SetDelay(0.3); //.SetEase(easetype).SetLoops(numberOfLoop, loopType).From();
+            //item.transform.DOMove((item.transform.position + to), 2).SetDelay(0.2f).SetEase(Ease.InOutSine).SetLoops(4, LoopType.Yoyo);
+
+            //item.transform.DOPunchPosition(to, 3, 0, 0.1f).SetDelay(0.3f).SetEase(Ease.InOutSine);
+            StartCoroutine(GainWind());
         }
+    }
+
+    IEnumerator GainWind()
+    {
+        Debug.Log("wewee");
+        yield return new WaitForSeconds(0.1f);
+        Player.Air += AirGain;
+
     }
 
 }
