66a06639e1a4bbb05cf3282a09be972565a5799e
Theonojutsu <tbirlian@gmail.com>
2023-02-05 15:51:17 -0500

    Théo

---
+++ GameJam/Assets/Théo/CharSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharSpawner : MonoBehaviour
+{
+    Camera cam;
+    public GameObject prefab;    
+    public float spawnInterval = 5f;
+    private float spawnTimer = 0f;
+    private Vector3 posChar = new(0f,0.2f,0f);
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 screenPoint = cam.WorldToViewportPoint(posChar);
+
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= spawnInterval)
+        {
+            spawnTimer = 0f;
+            Instantiate(prefab, screenPoint, Quaternion.identity);
+        }
+    }
+}
--- GameJam/Assets/Théo/Characters.cs
+++ GameJam/Assets/Théo/Characters.cs
@@ -5,140 +5,78 @@ using DG.Tweening;
 
 public class Characters : MonoBehaviour
 {
-    public Camera cam;
+    #region Variables
+
+    private Camera cam;
     public float speed = 1.0f;
     public Vector3 direction = Vector3.zero;
+    public Vector3 endValue;
     public Rigidbody2D charRigid;
+    public Sprite[] sprites;
+    private SpriteRenderer spriteRenderer;
+    public GameObject prefab;
+
+    private bool isDottedWeen = false;
+    public float duration = 5f;
+    public float duration2 = 6f;
+    public float strenght = 1;
+    public int vibrato = 10;
+    public float randomness = 90;
+    public bool snapping = false;
+    public bool fadeOut = true;
+
+   #endregion Variables
 
     void Start()
     {
+        cam = Camera.main;
         direction = Random.onUnitSphere;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        int randomIndex = Random.Range(0, sprites.Length);
+        spriteRenderer.sprite = sprites[randomIndex];
     }
 
     void Update()
     {
+        transform.position += direction * speed * Time.deltaTime;
+        direction = (direction + Random.onUnitSphere * 0.1f).normalized;
 
-            transform.position += direction * speed * Time.deltaTime;
-            direction = (direction + Random.onUnitSphere * 0.1f).normalized;
+        Vector3 screenPoint = cam.WorldToViewportPoint(transform.position);
 
-            Vector3 screenPoint = cam.WorldToViewportPoint(transform.position);           
+        if (screenPoint.x > 1)
+        {
+            screenPoint.x = 0;
+            transform.position = cam.ViewportToWorldPoint(screenPoint);
+        }
 
-            if (screenPoint.x > 1)
-            {
-                screenPoint.x = 0;
-                transform.position = cam.ViewportToWorldPoint(screenPoint);
-            }
-            else if (screenPoint.x < 0)
-            {
-                screenPoint.x = 1;
-                transform.position = cam.ViewportToWorldPoint(screenPoint);
-            }
-            if (screenPoint.y >= 1.1)
-            {
-                direction = -direction;
-            }
-            else if (screenPoint.y <= 0.5)
-            {
-                direction = -direction;
-            }
+        else if (screenPoint.x < 0)
+        {
+            screenPoint.x = 1;
+            transform.position = cam.ViewportToWorldPoint(screenPoint);
+        }
 
-            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        if (screenPoint.y >= 1.1)
+        {
+            direction = -direction;
+        }
+
+        else if ((screenPoint.y <= 0.5)&&(isDottedWeen == false))
+        {
+            isDottedWeen = true;
+
+            endValue = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+
+            this.transform.DOShakePosition(duration, strenght, vibrato, randomness, snapping, fadeOut, ShakeRandomnessMode.Full);
+            this.transform.DOLocalMove(endValue, duration2, snapping);
+            this.spriteRenderer.DOFade(0, 3f).OnComplete(() => {
+                Destroy(gameObject);
+            });
+        }
+
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
     }
 }
-        
-
-        //transform.position += direction * speed * Time.deltaTime;
-        //direction = (direction + Random.onUnitSphere * 0.1f).normalized;
-
-        //Vector3 screenPoint = camera.WorldToViewportPoint(transform.position);
-        //if (screenPoint.x > 1 || screenPoint.x < 0 || screenPoint.y > 1 || screenPoint.y < 0)
-        //{
-        //    direction = -direction;
-        //}
 
 
-        //        transform.position += direction * speed * Time.deltaTime;
-        //        direction = (direction + Random.onUnitSphere * 0.1f).normalized;
-
-        //        Vector3 screenPoint = camera.WorldToViewportPoint(transform.position);
-        //        if (screenPoint.x > 1)
-        //        {
-        //            screenPoint.x = 0;
-        //            transform.position = camera.ViewportToWorldPoint(screenPoint);
-        //        }
-        //        else if (screenPoint.x < 0)
-        //        {
-        //            screenPoint.x = 1;
-        //            transform.position = camera.ViewportToWorldPoint(screenPoint);
-        //        }
-        //        if (screenPoint.y > 1)
-        //        {
-        //            screenPoint.y = 0;
-        //            transform.position = camera.ViewportToWorldPoint(screenPoint);
-        //        }
-        //        else if (screenPoint.y < 0)
-        //        {
-        //            screenPoint.y = 1;
-        //            transform.position = camera.ViewportToWorldPoint(screenPoint);
-        //        }
-
-        //    transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-
-        //    Vector3 center = new Vector3(0.5f, 0.5f, 0f);
-        //    Vector3 directionToCenter = center - screenPoint;
-        //    direction += directionToCenter * redirectionForce * Time.deltaTime;
-        //}
-
-        //transform.position += direction * speed * Time.deltaTime;
-        //direction = (direction + Random.onUnitSphere * 0.1f).normalized;
-
-        //Vector3 screenPoint = camera.WorldToViewportPoint(transform.position);
-        //if (screenPoint.x > 1 || screenPoint.x < 0 || screenPoint.y > 1 || screenPoint.y < 0)
-        //{
-        //    direction = -direction;
-        //}
 
 
-        //        transform.position += direction * speed * Time.deltaTime;
-        //        direction = (direction + Random.onUnitSphere * 0.1f).normalized;
-
-        //        Vector3 screenPoint = camera.WorldToViewportPoint(transform.position);
-        //        if (screenPoint.x > 1)
-        //        {
-        //            screenPoint.x = 0;
-        //            transform.position = camera.ViewportToWorldPoint(screenPoint);
-        //        }
-        //        else if (screenPoint.x < 0)
-        //        {
-        //            screenPoint.x = 1;
-        //            transform.position = camera.ViewportToWorldPoint(screenPoint);
-        //        }
-        //        if (screenPoint.y > 1)
-        //        {
-        //            screenPoint.y = 0;
-        //            transform.position = camera.ViewportToWorldPoint(screenPoint);
-        //        }
-        //        else if (screenPoint.y < 0)
-        //        {
-        //            screenPoint.y = 1;
-        //            transform.position = camera.ViewportToWorldPoint(screenPoint);
-        //        }
-
-        //    transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-
-        //    Vector3 center = new Vector3(0.5f, 0.5f, 0f);
-        //    Vector3 directionToCenter = center - screenPoint;
-        //    direction += directionToCenter * redirectionForce * Time.deltaTime;
-        //}  
-        
-//// Redirection vers l'intérieur lorsque la mouche se rapproche du sol
-            //if (screenPoint.y < bottomLimit)
-            //{
-            //    direction = Vector3.Lerp(direction, -direction, (bottomLimit - screenPoint.y) / bottomLimit);
-            //}
-
-            //// Redirection vers l'intérieur lorsque la mouche se rapproche du plafond
-            //if (screenPoint.y > topLimit)
-            //{
-            //    direction = Vector3.Lerp(direction, -direction, (screenPoint.y - topLimit) / topLimit);
-            //}
